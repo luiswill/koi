ab9608dc9d8b48bdd9596c585c57f267b786720a
Elo20012 <elias.willnat@gmail.com>
2020-04-16 20:49:58 +0100

    Youtube Update

--- Main.gd
+++ Main.gd
@@ -5,3 +5,6 @@ func _ready():
 	print("schau ab 3:00")
 	print("https://www.youtube.com/watch?v=msZw59Iln74")
 
+	print("Anderes Video :")
+	print("https://www.youtube.com/watch?v=_urHlep2P84&list=PLyZ-ek4GLru19YC0nC-iE5MP3f_UT-4Kk&index=11&t=0s")
+
