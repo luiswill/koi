519c402aa8a9532aa9892874ceeb6d3041caee5a
luiswill <luis.willnat.lw@gmail.com>
2020-04-17 19:38:27 +0200

    Added sqllite

--- Main.gd
+++ Main.gd
@@ -1,17 +1,53 @@
 extends Node
 
-var position = Vector3()
-var position2 = Vector3()
+const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns");
 
-func _ready():
-	position = Vector3(1.0, 1.0, 1.0)
-	position2 = Vector3(2.0, 2.0, 2.0)
+var item_list = [];
+
+func _ready() -> void:
+	# Create new gdsqlite instance
+	var db = SQLite.new();
 	
+	# Open item database
+	if (!open_database(db, "res://databases/kois/items.db")):
+		print("Failed opening database.");
+		return;
+	
+	# Get item list from db
+	var pots = db.fetch_array("SELECT * FROM potion ORDER BY id ASC")
+	
+	for pot in pots:
+		# Create new item from database
+		var item = {
+			'id': pot['id'],
+			'name': pot['name'],
+			'price': pot['price'],
+			'heals': pot['heals']
+		};
+		
+		# Add to item list
+		item_list.append(item);
+	
+	# Print all item
+	for i in item_list:
+		print("Item ", i.id, " (", i.name, ") $", i.price, " +", i.heals, "hp");
+
+func open_database(db : SQLite, path : String) -> bool:
+	if (path.begins_with("res://")):
+		# Open packed database
+		var file = File.new();
+		if (file.open(path, file.READ) != OK):
+			return false;
+		var size = file.get_len();
+		var buffers = file.get_buffer(size);
+		return db.open_buffered(path, buffers, size);
+	
+	# Open database normally
+	return db.open(path);
 	
 
-func buy_fish():
-	print("Bought fish.")
 
-	print("Anderes Video :")
-	print("https://www.youtube.com/watch?v=_urHlep2P84&list=PLyZ-ek4GLru19YC0nC-iE5MP3f_UT-4Kk&index=11&t=0s")
-
+func _on_Timer_timeout():
+	print("New fish will maybe come ..")
+	#choose_a_koi()
+	
