53bfb92349606f07d2546b2cd3ea4b19fb0172b3
luiswill <luis.willnat.lw@gmail.com>
2020-04-19 11:37:06 +0200

    Improved shop UI

--- shop/Shop.gd
+++ shop/Shop.gd
@@ -4,7 +4,7 @@ onready var button = preload("res://ui/buttonKoiItem.gd")
 onready var kois
 
 onready var button_template = preload("res://ui/buttonKoiItem.tscn")
-var button_instance
+var button_instance : Button
 
 var all_kois_container
 
@@ -17,10 +17,19 @@ func _ready():
 func load_all_buttons():
 	all_kois_container = get_node("all_kois_container")
 	
-	
 	for koi in GLOBAL.all_kois:
+		load_shop_button_for_koi(koi)
+		
+		
+
+
+func load_shop_button_for_koi(koi : Koi):
 		button_instance = button_template.instance()
-		button_instance.text = koi.get_name()
+		var button_texture_instance : TextureRect = button_instance.get_node("image")
+		var button_label_name_instance : Label = button_instance.get_node("name")
+		var button_label_price_instance : Label = button_instance.get_node("price")
+		
+		button_label_name_instance.text = koi.get_name()
+		button_label_price_instance.text = str(koi.get_price()) + GAME.currency
+		
 		all_kois_container.add_child(button_instance)
-		
-		
